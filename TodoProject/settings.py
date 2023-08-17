96a02442107b7705bcc3dae809700034741dd243
shamithmylar20 <shamithmylar321@gmail.com>
2023-08-18 04:56:05 +0530

    Final Update

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -79,16 +79,13 @@ WSGI_APPLICATION = 'TodoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'Todos',
-        # 'USER': 'shamith',
-        # 'PASSWORD': 'shamith10221',
-        # 'HOST': 'database-1.cf9dt6lrnclf.eu-north-1.rds.amazonaws.com',
-        # 'PORT': '5432',
+                
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Todos',
+        'USER': 'shamith',
+        'PASSWORD': 'shamith10221',
+        'HOST': 'project-1.cf9dt6lrnclf.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
